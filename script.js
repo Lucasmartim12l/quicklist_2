aa7af27957f08170ab4900361b8cd9be9dbdb0a6
Lucasmartim12l <lucas.martim@eaportal.org>
2025-06-10 12:07:01 +0000

    atualização e correção dos codigos do HTML, CSS e JS

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
 let items = []
 
-function addItem () {
+function addItem() {
     const itemName = document.querySelector("#item").value
 
-    if (itemName === "") {
-       alert("Digite um item válido!") 
-       return
+    if (itemName === ""){
+        alert("Digite um item válido!")
+        return
     }
 
-   const item = {
-    name: itemName,
-    checked: false 
-   }
+    const item = {
+        name: itemName,
+        checked: false
+    }
 
-   items.push(item)
-   document.querySelector("#item").value = ""
+    items.push(item)
 
-   showItemsList()
+    document.querySelector("#item").value = ""
+
+    showItemsList()
 }
+
+document.querySelector("#item").addEventListener("keydown", (event) => {
+    if(event.key === "Enter"){
+        addItem()
+    }
+})
 
 function showItemsList() {
     const sectionList = document.querySelector(".list")
     sectionList.textContent = ""
 
-    items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked) )
+    items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
 
-    
-    items.map((item , index) => {
-        sectionList.innerHTML += ` 
-        <div class="item">
-            <div>
-                <input type="checkbox" name="list" id="item-${index}" ${item.checked && "checked"}>
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+             <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}" ${item.checked && "checked"}>
 
-                 <div class="custom-checkbox" onclick="checkItem('${item.name}')">
-                    <img src="./assets/checked.svg" alt="checked">
-                     <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
-                 </div>
+
+                    <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+                        <img src="./assets/checked.svg" alt="checked">
+                    </div>
+
+                    <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+                </div>
+
                 <button onclick="removeItem('${item.name}')">
-                    <img src="./assets/trash-icon.svg" alt="trash-icon">
+                    <img src="./assets/trash-icon.svg" alt="trash icon">
                 </button>
             </div>
-          </div>
         `
     })
 
@@ -52,11 +61,13 @@ function removeItem(itemName) {
     const divWarning = document.querySelector(".warning")
 
     divWarning.classList.remove("hide-warning")
-    setTimeout(() =>{ 
+
+    setTimeout(() => {
         divWarning.classList.add("hide-warning")
     }, 4000)
 
-    if (itemIndex !== -1) {
+
+    if (itemIndex !== -1){
         items.splice(itemIndex, 1)
     }
 
@@ -67,18 +78,37 @@ function addHideWarningClass() {
     document.querySelector(".warning").classList.add("hide-warning")
 }
 
-function checkItem(itemName) {
-   const item = items.find((item) => item.name === itemName)
-   item.checked = !item.checked
-   showItemsList()
-}
+function checkItem(itemName){
+    const item = items.find((item) => item.name === itemName)
 
-function verifyLocalStorageItems() {
-   const LocalStorageItems = localStorage.getItem("items") 
-   if (LocalStorageItems) {
-    items = JSON.parse(LocalStorageItems)
+    /*
+    
+    //if/else simples:
+    
+        if(item.checked === true){
+        item.checked = false
+      } else {
+        item.checked = true
+     }
+
+    //if ternário
+
+       item.checked === true ? item.checked = false : item.checked = true
+    */
+   
+    item.checked = !item.checked
     showItemsList()
-   }
 }
 
-verifyLocalStorageItems()
+function verifyLocalStorageItems () {
+    const localStorageItems = localStorage.getItem("items")
+
+    if (localStorageItems) {
+        items = JSON.parse(localStorageItems)
+        showItemsList()
+    }
+}
+
+verifyLocalStorageItems ()
+
+
